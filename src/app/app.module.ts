812ba2bbcf94d5aa61ef2e238b93197476baea90
Alick Buscariolli <6041@ait.nsw.edu.au>
2019-05-26 14:13:30 +1000

    Login system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FIREBASE_CONFIG } from './app.firebase.config';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -5,6 +6,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +15,12 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+            IonicModule.forRoot(),
+            AppRoutingModule,
+            AngularFireModule.initializeApp(FIREBASE_CONFIG),
+            AngularFireAuthModule
+          ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/register/register.page.ts
@@ -0,0 +1,29 @@
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from './../../models/user';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  user = {} as User;
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  async register(user: User) {
+    try {
+      const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+      console.log(result);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+}
